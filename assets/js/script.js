49440a46cf714b2cb5b3b53b83d448c7e0ab55e1
tenstand <christensmith_@msn.com>
2022-02-15 14:35:45 +0000

    Add code to calculate two random numbers in the game

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+        if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked submit!");
+        } else {
+            let gameType = this.getAttribute("data-type");
+            alert("You clicked $(gameType)");
+        }       
+        })
+
+
+)}
+
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
